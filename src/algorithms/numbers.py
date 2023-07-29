81554b0cedbdfc35145ca915e8a71b9f5e0c9405
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-07-29 16:17:26 +0100

    Update src/algorithms/numbers.py

--- src/algorithms/numbers.py
+++ src/algorithms/numbers.py
@@ -1,21 +1,26 @@
 import secrets
 import numpy as np
+import pandas as pd
 
 
 class Numbers:
 
     def __init__(self) -> None:
-        
+
+
+        self.__size = (16, 1)
         constant = 256
         self.__variable = secrets.randbits(k=constant)
         self.__constant = constant
 
-    def __series_reproducible(self):
+    def series_reproducible(self) -> np.ndarray:
 
         generator = np.random.default_rng(seed=self.__constant)
 
-        return generator.random(size=(16, 1))
+        return generator.random(size=self.__size)
+    
+    def series_variable(self) -> pd.DataFrame:
 
-    def exc(self) -> np.ndarray:
+        generator = np.random.default_rng(seed=self.__variable)
 
-        return self.__series_reproducible()
+        return pd.DataFrame(data={'number': generator.random(size=self.__size).squeeze()})
