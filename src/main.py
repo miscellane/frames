210017901f13226f79989719e823c739b79b1b59
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-07-29 15:45:43 +0100

    Add src/main.py

---
+++ src/main.py
@@ -0,0 +1,27 @@
+import logging
+import os
+import sys
+
+def main():
+
+    logger.info('frames')
+
+    numbers = src.algorithms.numbers.Numbers()
+    logger.info(numbers.exc())
+
+
+if __name__ == '__main__':
+    root = os.getcwd()
+    sys.path.append(root)
+    sys.path.append(os.path.join(root, 'src'))
+
+    # Logging
+    logging.basicConfig(level=logging.INFO,
+                        format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    logger = logging.getLogger(__name__)
+
+    # Libraries
+    import src.algorithms.numbers
+
+    main()
